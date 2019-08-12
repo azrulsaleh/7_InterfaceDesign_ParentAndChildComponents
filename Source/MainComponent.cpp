a10f09d18ad892c2953d12e7dbd63a013cdb117e
Azrul Saleh <azrulsaleh@me.com>
2019-08-11 22:16:39 -0700

    Completed tutorial

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -8,30 +8,164 @@
 
 #include "MainComponent.h"
 
+SunComponent::SunComponent()
+{
+    
+}
+SunComponent::~SunComponent()
+{
+    
+}
+void SunComponent::paint (Graphics& g)
+{
+    g.setColour(Colours::yellow);
+    auto lineThickness = 3.0f;
+    double radius = getWidth() < getHeight() ? radius = getWidth() : radius = getHeight();
+    g.drawEllipse(lineThickness * 0.5f,
+                  lineThickness * 0.5f,
+                  radius - lineThickness,
+                  radius - lineThickness,
+                  lineThickness);
+}
+void SunComponent::resized()
+{
+    
+}
+//==============================================================================
+WallComponent::WallComponent()
+{
+    
+}
+WallComponent::~WallComponent()
+{
+    
+}
+void WallComponent::paint (Graphics& g)
+{
+    g.fillCheckerBoard(getLocalBounds().toFloat(), 30, 10, Colours::sandybrown, Colours::saddlebrown);
+}
+void WallComponent::resized()
+{
+    
+}
+//==============================================================================
+RoofComponent::RoofComponent()
+{
+    
+}
+RoofComponent::~RoofComponent()
+{
+    
+}
+void RoofComponent::paint (Graphics& g)
+{
+    g.setColour(Colours::red);
+    Path roof;
+    roof.addTriangle(0, getHeight(), getWidth(), getHeight(), getWidth() / 2, 0);
+    g.fillPath(roof);
+}
+void RoofComponent::resized()
+{
+    
+}
+//==============================================================================
+FloorComponent::FloorComponent()
+{
+    
+}
+FloorComponent::~FloorComponent()
+{
+    
+}
+void FloorComponent::paint (Graphics& g)
+{
+    g.setColour(Colours::green);
+    g.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2, 5);
+}
+void FloorComponent::resized()
+{
+    
+}
+//==============================================================================
+HouseComponent::HouseComponent()
+{
+    addAndMakeVisible(wall);
+    addAndMakeVisible(roof);
+}
+HouseComponent::~HouseComponent()
+{
+    
+}
+void HouseComponent::paint (Graphics& g)
+{
+    
+}
+void HouseComponent::resized()
+{
+    auto separation = jlimit (2, 10, getHeight() / 20);
+    roof.setBounds(0, 0, getWidth(), (getHeight() * 0.2) - (separation / 2));
+    wall.setBounds(0, getHeight() * 0.20 + separation / 2, getWidth(), (getHeight() * 0.80) - separation);
+}
+//==============================================================================
+StreetComponent::StreetComponent()
+{
+    addAndMakeVisible(house1);
+    addAndMakeVisible(house2);
+    addAndMakeVisible(house3);
+}
+StreetComponent::~StreetComponent()
+{
+    
+}
+void StreetComponent::paint (Graphics& g)
+{
+
+}
+void StreetComponent::resized()
+{
+    house1.setBounds(0, 0, 50, 50);
+    house2.setBounds(0, 70, 50, 50);
+    house3.setBounds(0, 140, 50, 50);
+}
+//==============================================================================
+SceneComponent::SceneComponent()
+{
+    addAndMakeVisible(floor);
+    addAndMakeVisible(house);
+    addAndMakeVisible(smallHouse);
+    addAndMakeVisible(street);
+    addAndMakeVisible(sun);
+}
+SceneComponent::~SceneComponent()
+{
+    
+}
+void SceneComponent::paint (Graphics& g)
+{
+    g.fillAll (Colours::lightblue);
+}
+void SceneComponent::resized()
+{
+    floor.setBounds(10, 297, 580, 5);
+    house.setBounds(300, 70, 200, 220);
+    smallHouse.setBounds(50, 70, 50, 50);
+    street.setBounds(180, 70, 50, 300);
+    sun.setBounds(530, 10, 60, 60);
+}
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(scene);
     setSize (600, 400);
 }
-
 MainComponent::~MainComponent()
 {
 }
-
-//==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    scene.setBounds(getLocalBounds());
 }
-
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    scene.setBounds(getLocalBounds());
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,26 +10,99 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+class SunComponent    : public Component
+{
+public:
+    SunComponent();
+    ~SunComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SunComponent)
+};
 //==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
+class WallComponent    : public Component
+{
+public:
+    WallComponent();
+    ~WallComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WallComponent)
+};
+//==============================================================================
+class RoofComponent    : public Component
+{
+public:
+    RoofComponent();
+    ~RoofComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RoofComponent)
+};
+//==============================================================================
+class FloorComponent    : public Component
+{
+public:
+    FloorComponent();
+    ~FloorComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FloorComponent)
+};
+//==============================================================================
+class HouseComponent    : public Component
+{
+public:
+    HouseComponent();
+    ~HouseComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    WallComponent wall;
+    RoofComponent roof;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HouseComponent)
+};
+//==============================================================================
+class StreetComponent    : public Component
+{
+public:
+    StreetComponent();
+    ~StreetComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    HouseComponent house1, house2, house3;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StreetComponent)
+};
+//==============================================================================
+class SceneComponent    : public Component
+{
+public:
+    SceneComponent();
+    ~SceneComponent();
+    void paint (Graphics&) override;
+    void resized() override;
+private:
+    FloorComponent floor;
+    HouseComponent house;
+    HouseComponent smallHouse;
+    StreetComponent street;
+    SunComponent sun;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SceneComponent)
+};
+//==============================================================================
 class MainComponent   : public Component
 {
 public:
-    //==============================================================================
     MainComponent();
     ~MainComponent();
-
-    //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
-
 private:
-    //==============================================================================
-    // Your private member variables go here...
-
-
+    SceneComponent scene;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
